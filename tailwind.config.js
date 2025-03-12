253812ba459236de54382ccdf6916dd991c9461b
sajjan <sajjanpaudel811@gmail.com>
2025-03-12 09:14:51 +0545

    fix(ux): do not show payer and ower unless group is selected

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'slide-down': {
+          '0%': { opacity: '0', transform: 'translateY(-10px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        'slide-down': 'slide-down 0.5s ease-in-out forwards',
+        'slide-down-delay': 'slide-down 0.5s ease-in-out 0.3s forwards'
+      }
+    },
   },
   plugins: [],
 }

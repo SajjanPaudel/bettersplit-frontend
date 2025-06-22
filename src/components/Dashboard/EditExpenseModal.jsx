3774a4d3379c0f429f0e3e8babe071d22b1d1d6b
sajjan <sajjanpaudel811@gmail.com>
2025-06-22 15:25:30 +0545

    fix: height of modal

--- src/components/Dashboard/EditExpenseModal.jsx
+++ src/components/Dashboard/EditExpenseModal.jsx
@@ -184,7 +184,7 @@ function EditExpenseModal({ onClose, onSuccess, initialExpense, activityId }) {
       onClick={handleBackdropClick}
     >
       <div className={`${theme.card} rounded-2xl border ${theme.border} w-full max-w-2xl my-4 lg:my-0 shadow-2xl relative before:absolute before:inset-0 before:bg-gradient-to-b  before:rounded-2xl before:pointer-events-none`}>
-        <div className="p-6 lg:p-8 relative">
+        <div className="p-6 lg:p-8 relative max-h-[90vh] overflow-y-auto">
           <div className="flex justify-between items-center mb-3">
             <button
               onClick={onClose}
@@ -369,7 +369,7 @@ function EditExpenseModal({ onClose, onSuccess, initialExpense, activityId }) {
             </div>
             <h3 className={`${theme.text} px-2`}>Split Details</h3>
             {selectedUsers.length > 0 && (
-            <div className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4`}>
+              <div className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 max-h-[40vh] overflow-y-auto`}>
                 {expense.splits.map((split, index) => (
                   <div key={index} className="flex items-center justify-between gap-3 px-3 py-2 rounded-xl bg-green-800/50 text-white/70">
                     <div className={`w-1/2 ${theme.text} px-6 py-2 rounded-xl ${theme.input} border ${theme.inputBorder}`}>
